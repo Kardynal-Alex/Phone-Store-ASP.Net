a235fce1851b78157af202ae4e5e9ca66b108935
Kardynal-Alex <alexandrkardynal@gmail.com>
2020-11-26 19:51:30 +0200

    New DataBase

--- PhoneShop/Models/DataModel/ContactDetail.cs
+++ PhoneShop/Models/DataModel/ContactDetail.cs
@@ -1,9 +1,14 @@
-﻿namespace PhoneShop.Models.DataModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneShop.Models.DataModel
 {
     public class ContactDetail
     {
         public int Id { get; set; }
+        [Display(Name = "Supplier Name")]
+        [Required(ErrorMessage = "Enter Name Supplier")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter Phone Supplier")]
         public int Phone { get; set; }
     }
 }
--- PhoneShop/Models/DataModel/Supplier.cs
+++ PhoneShop/Models/DataModel/Supplier.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhoneShop.Models.DataModel
 {
     public class Supplier
     {
         public int Id { get; set; }
+        [Display(Name = "Name of Corporation")]
+        [Required(ErrorMessage = "Enter Name Corporation")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter Country Supplier")]
         public string Country { get; set; }
         public int ContactDetailId { get; set; }
         public ContactDetail ContactDetail { get; set; }
